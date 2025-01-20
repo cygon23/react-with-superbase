4a31517523e4abc4bedb600c998f2601510440c0
cygon23 <rweyemamu088@gmail.com>
2025-01-20 16:18:47 +0300

    fetching data from superbase complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,43 @@
+import React from "react"
+import supabase from "../config/superBaseClient"
+import { useEffect, useState } from "react"
+
+//components
+import SuperBaseData from "../components/SuperBaseData";
+
 const Home = () => {
+  const [fetchError, setFetchError] = useState(null)
+  const [store, setStore] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from('project').select()
+      
+      if (error) {
+        setFetchError('colud not actually fetch the data')
+        setStore(null)
+        console.log(error)
+      }
+
+      if (data) {
+        setStore(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchData() 
+  },[])
+  
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && (<p>{fetchError}</p>)}
+      {store && (<div className="smoothies">
+        <div className="smoothie-grid">
+            {store.map(item => (
+          <SuperBaseData key={item.id} data={item} />
+        ))}
+       </div>
+      </div>)}
     </div>
   )
 }
