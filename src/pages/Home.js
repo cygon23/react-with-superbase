10699d12b992b91737eca2f5c5e258d347637728
cygon23 <rweyemamu088@gmail.com>
2025-01-21 14:22:52 +0300

    update,delete,and ordering using superBase method complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,19 @@ import SuperBaseData from "../components/SuperBaseData";
 const Home = () => {
   const [fetchError, setFetchError] = useState(null)
   const [store, setStore] = useState(null)
+  const [orderBy, setOrderBy] = useState('created_at')
+
+  //updating the local state
+  const handleDelete = (id) => {
+    setStore(prevData => {
+      return prevData.filter(pd => pd.id !== id)
+    })
+  }
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data, error } = await supabase.from('project').select()
+      const { data, error } = await supabase.from('project')
+        .select().order(orderBy, {ascending: false})
       
       if (error) {
         setFetchError('colud not actually fetch the data')
@@ -26,15 +35,26 @@ const Home = () => {
     }
 
     fetchData() 
-  },[])
+  },[orderBy])
   
   return (
     <div className="page home">
       {fetchError && (<p>{fetchError}</p>)}
       {store && (<div className="smoothies">
+        <div className="order-by">
+          <p>Order by:</p>
+          <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+          <button onClick={() => setOrderBy('title')}>Title</button>
+          <button onClick={() => setOrderBy('rate')}>Rating</button>
+        </div>
         <div className="smoothie-grid">
             {store.map(item => (
-          <SuperBaseData key={item.id} data={item} />
+              <SuperBaseData
+                key={item.id}
+                data={item}
+                //passing the prop
+                onDelete={handleDelete}
+              />
         ))}
        </div>
       </div>)}
