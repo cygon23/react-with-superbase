bde1b7f2cba00d07e07e2173d8a3e0e8c245e1ff
cygon23 <rweyemamu088@gmail.com>
2025-01-22 10:50:05 +0300

    auth implementaion

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import Create from "./pages/Create"
 import Update from "./pages/Update"
+import Signup from "./pages/Signup"
+import Login from "./pages/Login"
+
 
 
 function App() {
@@ -14,8 +17,11 @@ function App() {
         <h1>CRUD React and SuperBase</h1>
         <Link to="/">Home</Link>
         <Link to="/create">Fast execution</Link>
+        <Link to="/logout">Logout</Link>
       </nav>
       <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/:id" element={<Update />} />
